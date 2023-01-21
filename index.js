c659b0e6989c975b3a3dc7c20920f8cfb86c12f0
Dilynn Strickland <870Gaming@gmail.com>
2023-01-20 18:39:30 -0600

    app configured and ready to develop

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.static("public", {index: "index.html", extensions: ["html"]}));
+
+// The maximum request body size is 100 kilobytes; however, my word list was
+// ~150kb. So I just doubled the request body size limit
+app.use(express.json({limit: '200kb'}));
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on ${process.env.PORT}`);
+});
