48da1848844d453edee2aa0641ba0a03ce8cfec0
Braden <bradenjackson2001@gmail.com>
2023-02-13 12:55:37 -0600

    Random changes

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const db = require("./db");
+const crypto = require("crypto");
+const argon2 = require("argon2");
+
+async function createUser(email, username, firstname, lastname, password) {
+    let created; 
+    const uuid = crypto.randomUUID();
+    const PasswordHash = await argon2.hash(password);
+
+    const sql = `
+        INSERT INTO Users
+        values (@userID, @username, @passowrdHash, @email, @firstname, @lastname)
+    `;
+
+    const stmt = db.prepare(sql);
+
+    try {
+        stmt.run({
+            userID: uuid,
+            username: username,
+            passwordHash: PasswordHash,
+            email: email,
+            firstname: firstname,
+            lastname: lastname,
+        });
+        created = true;
+    } catch(err) { // catch if user exists
+        console.error(err);
+        created = false;
+    }
+
+    return created;
+}
+
+exports.createUser = createuser;
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 "use strict";
-
 require("dotenv").config();
+
+const redis = require("redis");
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
+
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded( {extended: false }));
 app.use(express.static("public", {index: "index.html", extensions: ["html"]}));
 
 // The maximum request body size is 100 kilobytes; however, my word list was
